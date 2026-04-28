0f2e99238895efabf4f4dd3eb7d6cbe305299f1d
Gabriel Chalmel Toussaint <gchalmel@f4r7s12.clusters.42paris.fr>
2026-04-28 17:09:04 +0200

    Error handling for parsing ext

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 16:04:14 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/04/28 16:44:39 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/04/28 17:03:56 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	ft_parse(const char *filename, t_data *data)
 {
 	int	size_filename;
 
+	(void)data;
 	size_filename = ft_strlen(filename);
 	printf("%s\n", &filename[size_filename - 4]);
 	if (ft_strncmp(&filename[size_filename - 4], ".cub", 5))
 	{
-		printf("Error\n");
-		return ;
+		exit(ft_error("parse.c", ERR_PARSER_EXTENSION));
 	}
 }
