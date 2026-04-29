1838ba39b12785919f4bf8144ca6f5010ac8ed1e
Gabriel Chalmel Toussaint <gchalmel@f4r7s12.clusters.42paris.fr>
2026-04-29 14:35:46 +0200

    change ft_isspace

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 16:04:14 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/04/29 14:27:47 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/04/29 14:35:16 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@ void	ft_init_param(t_fileinfo *fi)
 	fi->we_path = NULL;
 }
 
-int	ft_isspace(char *line, int i)
+int	ft_isspace(char c)
 {
-	while ((line[i] != '\0') && (line[i] == ' ' || line[i] == '\f'
-			|| line[i] == '\n' || line[i] == '\r'
-			|| line[i] == '\t' || line[i] == '\v'))
-		i++;
-	return (i);
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v')
+		return (1);
+	return (0);
 }
 
 void	ft_parse(const char *filename, t_data *data)
@@ -47,7 +46,8 @@ void	ft_parse(const char *filename, t_data *data)
 	while (line != NULL)
 	{
 		i = 0;
-		i = ft_isspace(line, i);
+		while (line[i] != '\0' && ft_isspace(line[i]))
+			i++;
 		if (!ft_strncmp(&line[i], "NO", 3))
 			ft_fill_no();
 		else if (!ft_strncmp(&line[i], "SO", 3))
