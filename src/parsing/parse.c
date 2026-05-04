72a1b3b232e943d82e5284e69e6a4a798a5cb562
Gabriel Chalmel Toussaint <gchalmel@f4r7s12.clusters.42paris.fr>
2026-05-04 17:05:38 +0200

    add new parse spawn

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 16:04:14 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/05/04 15:25:14 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/05/04 17:04:03 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,5 @@ void	ft_parse(const char *filename, t_data *data)
 		exit(ft_error("parse.c", ERR_PARSER_EXTENSION));
 	data->map_size_y = ft_loop_parse(filename, line, data);
 	parse_map(data, data->map_size_y, filename);
+	parse_spawn(data);
 }
---
+++ src/parsing/parse_spawn.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_spawn.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 16:43:30 by gchalmel          #+#    #+#             */
+/*   Updated: 2026/05/04 17:02:16 by gchalmel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+#include "../../libft/libft.h"
+#include <stdio.h>
+
+void	fill_data(t_data *data, int x, int y)
+{
+	if (data->player->direction != '\0')
+		exit(ft_error("parse_spawn.c", ERR_PARSER_MANY_SPAWN));
+	data->player->posX = x;
+	data->player->posY = y;
+	data->player->direction = data->map[y][x];
+	data->map[y][x] = '0';
+}
+
+void	parse_spawn(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	data->player->direction = '\0';
+	while (i < data->map_size_y)
+	{
+		while (data->map[i][j] != '\0')
+		{
+			if (data->map[i][j] == 'N')
+				fill_data(data, j, i);
+			else if (data->map[i][j] == 'W')
+				fill_data(data, j, i);
+			else if (data->map[i][j] == 'E')
+				fill_data(data, j, i);
+			else if (data->map[i][j] == 'S')
+				fill_data(data, j, i);
+			j++;
+		}
+		i++;
+	}
+	if (data->player->direction == '\0')
+		exit(ft_error("parse_spawn.c", ERR_PARSER_NO_SPAWN));
+}
