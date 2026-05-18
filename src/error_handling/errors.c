b76333cd8883c9379af8270388331a9c0cf617cf
Gabriel Chalmel Toussaint <gchalmel@f4r7s12.clusters.42paris.fr>
2026-05-18 17:52:20 +0200

    Norminette fix errors.c

--- src/error_handling/errors.c
+++ src/error_handling/errors.c
@@ -6,40 +6,47 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 13:50:00 by kevlim            #+#    #+#             */
-/*   Updated: 2026/05/14 15:12:18 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/05/18 17:51:42 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
+void	ft_fill_err_buff(char **err)
+{
+	err[0] = ft_strdup("Pas assez d'argument: ./cub3d <filename.cub>");
+	err[1] = ft_strdup("L'extension du fichier n'est pas .cub");
+	err[2] = ft_strdup("Not start MLX");
+	err[3] = ft_strdup("Not create window");
+	err[4] = ft_strdup("Path doit commencer par ./ ou /");
+	err[5] = ft_strdup("Texture direction incorrect Ex: NO <PATH>.xmp");
+	err[6] = ft_strdup("Doublons de texture");
+	err[7] = ft_strdup("Aucune PATH preciser pour la texture");
+	err[8] = ft_strdup("Les space sont interdites dans le PATH");
+	err[9] = ft_strdup("KeyWord inconnu ou mal placer");
+	err[10] = ft_strdup("Format de nombre rgb incorrect");
+	err[11] = ft_strdup("Plusieurs point de spawn ont ete detectes");
+	err[12] = ft_strdup("Aucun spawn n'a ete defini");
+	err[13] = ft_strdup("Map is wrong");
+	err[14] = ft_strdup("Aucune floor color et/ou ceiling color defini");
+	err[15] = ft_strdup("Manque une couleur RGB");
+	err[16] = ft_strdup("Door 'D' found but no DO texture");
+	err[17] = ft_strdup("Map open on borders");
+	err[18] = ft_strdup("Map open: empty space neighbor");
+	err[19] = ft_strdup("Failed to normalize map line");
+	err[20] = ft_strdup("Could not open file");
+	err[21] = ft_strdup("Malloc failed");
+}
+
 int	ft_error(char *from, int code)
 {
-	char *err[22] =
-	{
-		"Pas assez d'argument: ./cub3d <filename.cub>",
-		"L'extension du fichier n'est pas .cub",
-		"Not start MLX",
-		"Not create window",
-		"Path doit commencer par ./ ou /",
-		"Texture direction incorrect Ex: NO <PATH>.xmp",
-		"Doublons de texture",
-		"Aucune PATH preciser pour la texture",
-		"Les space sont interdites dans le PATH",
-		"KeyWord inconnu ou mal placer",
-		"Format de nombre rgb incorrect",
-		"Plusieurs point de spawn ont ete detectes",
-		"Aucun spawn n'a ete defini",
-		"Map is wrong",
-		"Aucune floor color et/ou ceiling color defini",
-		"Il faut bien definir les couleurs rgb: R:xxx,G:xxx,B:xxx",
-		"Door 'D' found but no DO texture",
-		"Map open on borders",
-		"Map open: empty space neighbor",
-		"Failed to normalize map line",
-		"Could not open file",
-		"Malloc failed"
-	};
+	char	**err;
 
+	if (err == NULL)
+	{
+		err = malloc(sizeof(char *) * 22);
+		ft_fill_err_buff(err);
+	}
 	return (error_msg(from, err[code], code));
 }
 
