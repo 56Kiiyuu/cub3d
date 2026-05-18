6fd1d39bf8e625ba9b7eb75a3893ff461f9859d8
Gabriel Chalmel Toussaint <gchalmel@f4r7s12.clusters.42paris.fr>
2026-05-18 17:53:11 +0200

    Norminette fix init folder

--- src/init/init_textures.c
+++ src/init/init_textures.c
@@ -3,31 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   init_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kevlim <kevlim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 15:46:54 by kevlim            #+#    #+#             */
-/*   Updated: 2026/05/12 15:38:10 by kevlim           ###   ########.fr       */
+/*   Updated: 2026/05/18 17:52:46 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void load_xpm(t_data *data, t_img *tex, char *path)
+void	load_xpm(t_data *data, t_img *tex, char *path)
 {
 	printf("Textures PATH: [%s]\n", path);
-	tex->img = mlx_xpm_file_to_image(data->mlx, path, &tex->width, &tex->height);
+	tex->img = mlx_xpm_file_to_image(data->mlx, path,
+			&tex->width, &tex->height);
 	if (!tex->img)
 		clean_exit(data, error_msg("texture", "Failed to load XPM texture", 1));
 	tex->addr = mlx_get_data_addr(tex->img, &tex->pixel_bits,
-		&tex->len_line, &tex->endian);
+			&tex->len_line, &tex->endian);
 }
 
 void	init_textures(t_data *data)
 {
-	load_xpm(data, &data->texture[0], data->params.no_path); // 0 = NO
-	load_xpm(data, &data->texture[1], data->params.so_path); // 1 = SO
-	load_xpm(data, &data->texture[2], data->params.we_path); // 2 = WE
-	load_xpm(data, &data->texture[3], data->params.ea_path); // 3 = EA
+	load_xpm(data, &data->texture[0], data->params.no_path);
+	load_xpm(data, &data->texture[1], data->params.so_path);
+	load_xpm(data, &data->texture[2], data->params.we_path);
+	load_xpm(data, &data->texture[3], data->params.ea_path);
 	if (BONUS && data->params.do_path)
 		load_xpm(data, &data->texture[4], data->params.do_path);
 	else
