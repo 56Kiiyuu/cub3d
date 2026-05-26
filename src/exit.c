45ebe59e539a0667aa8719afff88e895c90b98e3
kevlim <kevlim@student.42.fr>
2026-05-26 17:10:14 +0200

    free leaks from mlx

--- src/exit.c
+++ src/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kevlim <kevlim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 13:54:24 by kevlim            #+#    #+#             */
-/*   Updated: 2026/05/20 17:35:10 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/05/26 16:42:47 by kevlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,18 @@ void	clean_exit(t_data *data, int code, char *line)
 	if (data->fd > 0)
 		free_gnl_stash(data->fd);
 	free_parse_data(data);
-	if (data->win && data->mlx)
-		mlx_destroy_window(data->mlx, data->win);
+	if (data->player)
+		free(data->player);
+	free_textures(data);
 	if (data->mlx)
 	{
+		if (data->win)
+			mlx_destroy_window(data->mlx, data->win);
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
 	}
-	if (data->player)
-		free(data->player);
-	ft_error("NE PAS SUPR CA SERT A FREE LA VAR ERR", 99);
+	if (code != 0)
+		ft_error("NE PAS SUPR CA SERT A FREE LA VAR ERR", 99);
 	exit(code);
 }
 
--- src/parsing/ft_line_handler.c
+++ src/parsing/ft_line_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_line_handler.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kevlim <kevlim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 18:07:36 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/05/20 17:26:05 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/05/26 17:06:49 by kevlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,42 +20,53 @@ void	skip_space(char *line, int *i)
 		*i += 1;
 }
 
-static void	handler(t_data *data, char *line, int *i, int *vn)
+int	handle_textures(t_data *data, char *line, int *i)
 {
-	skip_space(line, i);
-	if (!ft_strncmp(&line[*i], "NO", 2))
+	if (!ft_strncmp(&line[*i], "NO", 2) && !data->params.no_path)
 		data->params.no_path = ft_fill_data_info(data, &line[*i + 2], line);
-	else if (BONUS && !ft_strncmp(&line[*i], "DO", 2))
+	else if (BONUS && !ft_strncmp(&line[*i], "DO", 2) && !data->params.do_path)
 		data->params.do_path = ft_fill_data_info(data, &line[*i + 2], line);
-	else if (!ft_strncmp(&line[*i], "SO", 2))
+	else if (!ft_strncmp(&line[*i], "SO", 2) && !data->params.so_path)
 		data->params.so_path = ft_fill_data_info(data, &line[*i + 2], line);
-	else if (!ft_strncmp(&line[*i], "WE", 2))
+	else if (!ft_strncmp(&line[*i], "WE", 2) && !data->params.we_path)
 		data->params.we_path = ft_fill_data_info(data, &line[*i + 2], line);
-	else if (!ft_strncmp(&line[*i], "EA", 2))
+	else if (!ft_strncmp(&line[*i], "EA", 2) && !data->params.ea_path)
 		data->params.ea_path = ft_fill_data_info(data, &line[*i + 2], line);
-	else if (!ft_strncmp(&line[*i], "F", 1) && !vn[0])
-		parse_rgb(data, &data->params.floor_color, &line[*i + 1], line);
-	else if (!ft_strncmp(&line[*i], "C", 1) && !vn[0])
-		parse_rgb(data, &data->params.ceiling_color, &line[*i + 1], line);
-	else if ((!ft_strncmp(&line[*i], "1", 1)
-			|| !ft_strncmp(&line[*i], " ", 1)) && !vn[1])
-	{
-		vn[0] = 1;
-		data->map_size_y++;
-	}
-	else if (vn[0] && !vn[1])
-		vn[1] = 1;
-	else if ((!(line[*i] == '\n') && !(line[*i] == '\0')) || (vn[1] && vn[0]))
-		clean_exit(data, ft_error("parse.c", ERR_PARSER_BAD_KEYWORD), line);
+	else
+		return (0);
+	return (1);
 }
 
+void	ft_line_handler(t_data *data, char *line, int *i)
+{
+	skip_space(line, i);
+	if (handle_textures(data, line, i))
+		return ;
+	else if (!ft_strncmp(&line[*i], "F", 1) && !data->is_map)
+		parse_rgb(data, &data->params.floor_color, &line[*i + 1], line);
+	else if (!ft_strncmp(&line[*i], "C", 1) && !data->is_map)
+		parse_rgb(data, &data->params.ceiling_color, &line[*i + 1], line);
+	else if ((!ft_strncmp(&line[*i], "1", 1)
+			|| !ft_strncmp(&line[*i], " ", 1)) && !data->map_end)
+	{
+		data->is_map = 1;
+		data->map_size_y++;
+	}
+	else if (data->is_map && !data->map_end)
+		data->map_end = 1;
+	else if ((!(line[*i] == '\n') && !(line[*i] == '\0'))
+		|| (data->map_end && data->is_map))
+		clean_exit(data, ft_error("parse.c", ERR_PARSER_BAD_KEYWORD), line);
+}
 /*
 vn[0] = is_map (if one we are in the map)
-vn[1] = sp (if one whe have possibly \n or a bad keyword in the map parse)
-*/
+vn[1] = sp / data->map_end (if one whe have possibly \n 
+or a bad keyword in the map parse)
+
 void	ft_line_handler(t_data *data, char *line, int *i)
 {
 	static int	var_norminette[2] = {0, 0};
 
 	handler(data, line, i, var_norminette);
 }
+*/
