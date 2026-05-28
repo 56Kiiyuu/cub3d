d3bd57852e8ec73eb7c18ccb5fa95c210e2e0a3b
kevlim <kevlim@f1br3s8.paris.42.school>
2026-05-28 14:55:39 +0200

    fix letter end issue

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: kevlim <kevlim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 15:16:04 by gabch             #+#    #+#             */
-/*   Updated: 2026/05/26 16:59:25 by kevlim           ###   ########.fr       */
+/*   Updated: 2026/05/28 14:45:51 by kevlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,17 @@ void	process_map_line(t_data *data, char *line, int j, int *i)
 {
 	if (!data->is_map && (line[j] == '1' || line[j] == '0' || line[j] == ' '))
 		data->is_map = 1;
-	if (data->is_map && !data->map_end)
+	if (data->is_map)
 	{
-		if (line[j] == '1' || line[j] == '0' || ft_strchr("NSEWDO ", line[j]))
+		if (line[j] == '1' || line[j] == '0' || ft_strchr("NSEWDO", line[j]))
+		{
+			if (data->map_end)
+				clean_exit(data, ft_error("parse_map.c", ERR_PARSER_BAD_KEYWORD), NULL);
 			ft_add_map_line(data, line, (*i)++);
+		}
+		else if (*i > 0 && (line[j] == '\n' || line[j] == '\0'))
+			data->map_end = 1;
 	}
-	else if (data->is_map && (line[j] == '\n' || line[j] == '\0'))
-		data->map_end = 1;
 }
 
 void	parse_map(t_data *data, int size_map, const char *filename)
